dc22d095ef876d11b5e395dbd2991daa4d1b5423
colasguerit <colas.guerit@gmail.com>
2025-09-26 09:52:36 +0200

    Devoir fini

--- exercise1.mjs
+++ exercise1.mjs
@@ -1,14 +1,3 @@
-/**
- * Exercise 1 : String and Array Manipulation
- *
- * How to execute your code ?
- * Write your solution, then run the following command in your terminal:
- *      node exercise1.mjs
- */
-
-// Expected result:
-// [ 'Mobile phone', 'Laptop computer', 'Headphones', 'Smartwatch' ]
-
 const users = [
   { id: 1, name: "Alice", age: 28, specialty: "Marketing" },
   { id: 2, name: "Bob", age: 35, specialty: "Engineering" },
@@ -16,21 +5,36 @@ const users = [
   { id: 4, name: "David", age: 42, specialty: "Engineering" },
   { id: 5, name: "Eve", age: 29, specialty: "Design" },
 ];
+// Etape 1
+let userNames = [];
 
-// Your code here
-
-//etape1
-
-let usernames = []; 
-for (let i = 0; i < users.length ; i++) {
-  usernames.push(users[i].name);
+for (let i=0; i < users.length; i++) {
+    userNames.push(users[i].name);
 }
 
-console.log(usernames) ;
+console.log(userNames);
 
-//etape2
+//Etape 2
+let userEngineers = [];
 
-//etape3
+for (let i=0; i < users.length; i++) {
+    if (users[i].specialty === "Engineering") {
+        userEngineers.push(users[i]);
+    }
+}
+console.log(userEngineers);
 
-//etape4
+//Etape 3
+let userAges = 0;
+for (let i=0; i<users.length; i++){
+    userAges += users[i].age;
+}
+let averageAge = userAges / users.length;
+console.log('Average age:', averageAge);
+//Etape 4
+let userDescriptions = [];
+for (let i=0; i<users.length; i++){
+    userDescriptions.push(users[i].name + ' is ' + users[i].age + ' years old and specializes in ' + users[i].specialty + '.');
+}
+console.log(userDescriptions);
 
